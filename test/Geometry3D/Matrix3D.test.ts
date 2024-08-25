d6445dfcdf08ff64b9c378ed0a41dfac9f35781d
Hannah Ellis <hannahellis4242@gmail.com>
2024-08-25 09:25:15 +0100

    adding test for determinants

--- test/Geometry3D/Matrix3D.test.ts
+++ test/Geometry3D/Matrix3D.test.ts
@@ -1,4 +1,5 @@
 import { randomFloat } from "../utils";
+import { det2 } from "../../src/Geometry2D/Matrix2D";
 
 type Matrix3D = [
   [number, number, number],
@@ -22,6 +23,23 @@ const matrix3D = (
   [g, h, i],
 ];
 
+const det3 = ([[a, b, c], [d, e, f], [g, h, i]]: Matrix3D): number =>
+  a *
+    det2([
+      [e, f],
+      [h, i],
+    ]) -
+  b *
+    det2([
+      [d, f],
+      [g, i],
+    ]) +
+  c *
+    det2([
+      [d, e],
+      [g, h],
+    ]);
+
 describe("Matrix3D", () => {
   test("can create a matrix", () => {
     const a = randomFloat(-5000, 5000);
@@ -37,7 +55,6 @@ describe("Matrix3D", () => {
     const i = randomFloat(-5000, 5000);
 
     const m = matrix3D(a, b, c, d, e, f, g, h, i);
-
     expect(m[0][0]).toBe(a);
     expect(m[0][1]).toBe(b);
     expect(m[0][2]).toBe(c);
@@ -49,5 +66,24 @@ describe("Matrix3D", () => {
     expect(m[2][0]).toBe(g);
     expect(m[2][1]).toBe(h);
     expect(m[2][2]).toBe(i);
+  });
+  test("can get the determinant", () => {
+    const a = randomFloat(-5000, 5000);
+    const b = randomFloat(-5000, 5000);
+    const c = randomFloat(-5000, 5000);
+
+    const d = randomFloat(-5000, 5000);
+    const e = randomFloat(-5000, 5000);
+    const f = randomFloat(-5000, 5000);
+
+    const g = randomFloat(-5000, 5000);
+    const h = randomFloat(-5000, 5000);
+    const i = randomFloat(-5000, 5000);
+
+    const m = matrix3D(a, b, c, d, e, f, g, h, i);
+    const det = det3(m);
+    expect(det).toBe(
+      a * (e * i - f * h) + b * (f * g - d * i) + c * (d * h - e * g)
+    );
   });
 });
