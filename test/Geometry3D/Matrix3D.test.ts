9561dbbaf7e410f7b7cc563ecea21266d0452747
Hannah Ellis <hannahellis4242@gmail.com>
2024-08-25 09:59:25 +0100

    doing matrix vector multiplication

--- test/Geometry3D/Matrix3D.test.ts
+++ test/Geometry3D/Matrix3D.test.ts
@@ -1,5 +1,6 @@
 import { randomFloat } from "../utils";
 import { det2 } from "../../src/Geometry2D/Matrix2D";
+import Vector3D, { dot3, vector3D } from "../../src/Geometry3D/Vector3D";
 
 type Matrix3D = [
   [number, number, number],
@@ -39,6 +40,11 @@ const det3 = ([[a, b, c], [d, e, f], [g, h, i]]: Matrix3D): number =>
       [d, e],
       [g, h],
     ]);
+
+const MV3 = (
+  [[a, b, c], [d, e, f], [g, h, i]]: Matrix3D,
+  v: Vector3D
+): Vector3D => [dot3([a, b, c], v), dot3([d, e, f], v), dot3([g, h, i], v)];
 
 describe("Matrix3D", () => {
   test("can create a matrix", () => {
@@ -85,5 +91,30 @@ describe("Matrix3D", () => {
     expect(det).toBe(
       a * (e * i - f * h) + b * (f * g - d * i) + c * (d * h - e * g)
     );
+  });
+  test("can multiply a matrix by a vector", () => {
+    const a = randomFloat(-5000, 5000);
+    const b = randomFloat(-5000, 5000);
+    const c = randomFloat(-5000, 5000);
+
+    const d = randomFloat(-5000, 5000);
+    const e = randomFloat(-5000, 5000);
+    const f = randomFloat(-5000, 5000);
+
+    const g = randomFloat(-5000, 5000);
+    const h = randomFloat(-5000, 5000);
+    const i = randomFloat(-5000, 5000);
+
+    const x = randomFloat(-5000, 5000);
+    const y = randomFloat(-5000, 5000);
+    const z = randomFloat(-5000, 5000);
+
+    const m = matrix3D(a, b, c, d, e, f, g, h, i);
+    const v = vector3D(x, y, z);
+
+    const u = MV3(m, v);
+    expect(u[0]).toBe(a * x + b * y + c * z);
+    expect(u[1]).toBe(d * x + e * y + f * z);
+    expect(u[2]).toBe(g * x + h * y + i * z);
   });
 });
--- test/Geometry3D/Vector3D.test.ts
+++ test/Geometry3D/Vector3D.test.ts
@@ -1,8 +1,5 @@
 import { randomFloat } from "../utils";
-
-type Vector3D = [number, number, number];
-
-const vector3D = (a: number, b: number, c: number): Vector3D => [a, b, c];
+import Vector3D, { vector3D, dot3 } from "../../src/Geometry3D/Vector3D";
 
 describe("Vector3D", () => {
   test("can create a vector", () => {
@@ -15,5 +12,21 @@ describe("Vector3D", () => {
     expect(v[0]).toBe(a);
     expect(v[1]).toBe(b);
     expect(v[2]).toBe(c);
+  });
+
+  test("can get the dot product of two vectors", () => {
+    const a = randomFloat(-5000, 5000);
+    const b = randomFloat(-5000, 5000);
+    const c = randomFloat(-5000, 5000);
+
+    const d = randomFloat(-5000, 5000);
+    const e = randomFloat(-5000, 5000);
+    const f = randomFloat(-5000, 5000);
+
+    const u = vector3D(a, b, c);
+    const v = vector3D(d, e, f);
+
+    const result = dot3(u, v);
+    expect(result).toBe(a * d + b * e + c * f);
   });
 });
