b79bf5b8b138fdfc17301eb939d80792b8df5c00
Hannah Ellis <hannahellis4242@gmail.com>
2024-09-01 10:06:56 +0100

    start setting up for library

--- src/Geometry2D.ts
+++ src/Geometry2D.ts
@@ -1,4 +1,31 @@
 import {
+  add,
+  cross2,
+  div,
+  dot2,
+  fromTo,
+  mult,
+  origin2,
+  sub,
+  vector2,
+  xAxis2,
+  yAxis2,
+} from "./Geometry2D/Vector2D";
+const Geometry2D = {
+  vector2,
+  origin2,
+  xAxis2,
+  yAxis2,
+  add,
+  sub,
+  mult,
+  div,
+  fromTo,
+  dot2,
+  cross2,
+};
+export { Geometry2D };
+import {
   addVV,
   cross,
   div,
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,7 @@
-<<<<<<< HEAD
-import { Vector2D, Matrix2D } from "./Geometry2D";
-const Geometry2D = { Vector2D, Matrix2D };
-=======
-import {
-  add,
-  cross2,
-  div,
-  dot2,
-  fromTo,
-  mult,
-  origin2,
-  sub,
-  vector2,
-  xAxis2,
-  yAxis2,
-} from "./Geometry2D/Vector2D";
-const Geometry2D = {
-  vector2,
-  origin2,
-  xAxis2,
-  yAxis2,
-  add,
-  sub,
-  mult,
-  div,
-  fromTo,
-  dot: dot2,
-  cross: cross2,
-};
->>>>>>> 8509f3d (packaging)
-export { Geometry2D };
+import { x, y, z } from "./Coord";
+import { Geometry2D } from "./Geometry2D";
+import { Geometry3D } from "./Geometry3D";
+
+const Coord = { x, y, z };
+
+export { Coord, Geometry2D, Geometry3D };
